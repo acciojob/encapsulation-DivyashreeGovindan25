d141ab6f5586838d6314be4086b357e9b0eac197
HP <divyashreeharitha@gmail.com>
2024-01-15 12:18:24 +0530

    Version2

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,10 +2,14 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    void setter(String name){
+    public void setter(String name){
         this.name = name;
     }
-    String getter(){
+    public String getter(){
         return name;
+    }
+    @Override
+    public String toString() {
+        return "RWOnly [name=" + name + "]";
     }
 }
